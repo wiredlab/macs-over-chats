65c5cb4bb535cf52d77a80354145e1f98dbe9922
Dan White <dan@whiteaudio.com>
2020-07-05 14:23:56 -0500

    Print event info to terminal

--- flask/app/main/events.py
+++ flask/app/main/events.py
@@ -2,6 +2,10 @@ from flask import session
 from flask_socketio import emit, join_room, leave_room
 from .. import socketio
 
+import inspect
+
+def myname():
+    return(inspect.stack()[1][3])
 
 
 @socketio.on('trial_new', namespace='/trial')
@@ -12,7 +16,7 @@ def trial_new(message):
     Config is sent to clients."""
     room = session.get('room')
     name = session.get('name')
-    print('trial_new:', room, name, message)
+    print(myname(), room, name, message)
     emit('trial_new', message, room=room)
 
 
@@ -22,7 +26,7 @@ def trial_ready(message):
     Broadcast name to all clients."""
     room = session.get('room')
     name = session.get('name')
-    print('trial_ready:', room, name, message)
+    print(myname(), room, name, message)
     emit('trial_ready', {'name':name}, room=room)
 
 
@@ -32,8 +36,9 @@ def trial_start(message):
     Broadcast the message to all clients."""
     # the event itself is sufficient, so no message content
     room = session.get('room')
-    print('trial_start:', room, name, message)
-    emit('trial_start', {}, room=room)
+    name = session.get('name')
+    print(myname(), room, name, message)
+    emit('trial_start', {}, room=room, include_self=False)
 
 
 @socketio.on('trial_end', namespace='/trial')
@@ -42,7 +47,7 @@ def trial_end(message):
     Broadcast to clients so they know to stop transmitting."""
     room = session.get('room')
     emit('trial_end', message, room=room)
-    print(room, name, message)
+    print(myname(), room, name, message)
 
 
 @socketio.on('packet_receive', namespace='/trial')
@@ -52,7 +57,7 @@ def packet_receive(message):
     #timestamp
     room = session.get('room')
     emit('packet_receive', message, room=room)
-    print(room, name, message)
+    print(myname(), room, name, message)
 
 
 
@@ -65,7 +70,7 @@ def packet_send(message):
     room = session.get('room')
     name = session.get('name')
     #log room,name,frame
-    print(room, name, message)
+    print(myname(), room, name, message)
 
 
 @socketio.on('packet_ack', namespace='/trial')
@@ -76,7 +81,7 @@ def packet_ack(message):
     name = session.get('name')
     ack = message['ack']
     #log (room, name, ack)
-    print(room, name, message)
+    print(myname(), room, name, message)
 
 
 
@@ -94,7 +99,7 @@ def joined(message):
     name = session.get('name')
     join_room(room)
     emit('status', {'msg': session.get('name') + ' has entered the room.'}, room=room)
-    print(room, name, message)
+    print(myname(), room, name, message)
 
 
 @socketio.on('text', namespace='/trial')
@@ -104,7 +109,7 @@ def text(message):
     room = session.get('room')
     name = session.get('name')
     emit('message', {'msg': session.get('name') + ':' + message['msg']}, room=room)
-    print(room, name, message)
+    print(myname(), room, name, message)
 
 
 @socketio.on('left', namespace='/trial')
@@ -115,5 +120,5 @@ def left(message):
     name = session.get('name')
     leave_room(room)
     emit('status', {'msg': session.get('name') + ' has left the room.'}, room=room)
-    print(room, name, message)
+    print(myname(), room, name, message)
 
